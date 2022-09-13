ebce1c2097d6c5857891ee2c4c99758ca02782b4
Charles Bosquet <charles.bosquet@axyus.com>
2022-09-13 11:29:36 +0200

    In dev

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +6,5 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
-  constructor() { }
 
 }
